c4fef52f9bacc6c77aa8b0fb936609d243730d60
haesooKr <gotn676@gmail.com>
2020-03-24 00:42:24 -0400

    Create rot13.js

---
+++ rot13.js
@@ -0,0 +1,10 @@
+const rot13 = (str) => {
+  
+  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index     = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
+}
+
+module.exports = rot13;
