99d01ea86dc624ee2b0bd09a9a12cf648202aac6
haesooKr <gotn676@gmail.com>
2020-03-25 13:39:19 -0400

    Change points to be more specific

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ io.on('connection', socket => {
       io.to(user.room).emit('next', {
         timer: user.room.timer,
         turn: user.room.turn,
-        points: getUsersInRoom(user.room).map(user => [user.id, user.point]),
+        points: getUsersInRoom(user.room).map(user => [user.id, user.point[1]]),
         words: ['haesoo', 'dayhong', 'no']
       })
     }
